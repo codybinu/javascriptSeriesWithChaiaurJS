e3737e2dbc3b9566a8f54af6fafede0f50fcb15c
codybinu <binayak.bidyasagar99@gmail.com>
2024-01-02 23:05:22 +0530

    data types

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,1 +1,19 @@
 "use srict"; //treat all JS code as newer version
+
+// alert(3+3) // this will throw error as we are using node.js not browser.
+
+// datatypes
+    //primitive datatypes
+        //number => range 2 to power 53
+        //bigint
+        //sting => ""
+        //boolean => true/false
+        //null => standalone value
+        //undefined => no value assigned
+        //symbol => unique
+        
+
+        //object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
