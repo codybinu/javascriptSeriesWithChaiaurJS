4b82b23afeae54ad3285f316ab4deff2f0ea0929
codybinu <binayak.bidyasagar99@gmail.com>
2024-01-02 23:19:33 +0530

    03 conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33wer"
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+
+
+//"33" => 33
+//"33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=>false
+//""=> false
+//"Binayak" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
