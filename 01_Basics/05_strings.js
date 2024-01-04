c258b13a6e0c450450d1eff949869facbdc82ca8
codybinu <binayak.bidyasagar99@gmail.com>
2024-01-04 22:27:23 +0530

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Binayak"
+const repoCount = 17
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} nd my reo cunt is ${repoCount}`);
+
+const gameName = new String('Binayak')
--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -11,10 +11,10 @@ let userEmail;
 const id = Symbol('123') //typeOf --> symbol
 const anotherId = Symbol('123') //typeOf --> symbol
 
-console.log(id)
-console.log(anotherId)
+// console.log(id)
+// console.log(anotherId)
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 let bigNumber = 76234398478719n //typeOf --> bigint
 // #nonprimitives or reference types
@@ -29,4 +29,31 @@ const myFunction = function(){
     console.log("Hello World");
 }                                       //typeOf --> function
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//there are 2 types of memories
+    //1. stack (all Primitive datatype uses stack)
+    //2. Heap (all nonPrimitive datatype uses Heap)
+
+    let myCodingName = "codybinu"
+    let anotherName = myCodingName
+
+    anotherName = "bincypher"
+    console.log(`My Coding Name is ${myCodingName}`);
+    console.log(`My another Name is ${anotherName}`);
+
+    let userOne = {
+        email : "brb@bb.com",
+        upi : "brb@ybl"
+    }
+
+    let userTwo = userOne
+
+    userTwo.email = "rte@bb.com"
+
+    console.log(userOne.email);
+    console.log(userTwo.email);
