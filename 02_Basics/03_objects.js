3611150b404500c2ea602f6a93481d573cc86e49
codybinu <binayak.bidyasagar99@gmail.com>
2024-01-09 07:17:34 +0530

    objects continuing

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,14 +1,17 @@
 //singleton : when objects re created with literals they are not singleton[they ll have multiple instances] but, when created with constructors they are singleton.
-Object.create //this is constructor method, this ll create singleton object
+// Object.create //this is constructor method, this ll create singleton object
 
 
 //object literals
 
-const my
+const mySym = Symbol("key1")// this has to be defined to use the key in Json
+
+console.log(typeof mySym) 
 
 const JsUser = {
     name : "Binayak",
     "full name": "Binayak Bidyasagar",
+    [mySym]: "mykey1", // technique to declar the symbol key [part-2]
     age: 29,
     location: "hyderabad",
     email: "binayak@gmail.com",
@@ -16,3 +19,17 @@ const JsUser = {
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+console.log(typeof JsUser[mySym]);
+
+// Object.freeze(JsUser) //to restrict the change of the value of the object
+
+JsUser.greeting = function () {
+    console.log("Hello Js User")
+}
+JsUser.greetingTwo = function () {
+    console.log(`Hello Js User, from ${this["full name"]}`)
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,34 @@
+// const tinderUser = new Object(); //this kind of declaration of object will create a singleton object
+const tinderUser = {}; //this kind of declaration will create a non-singleton object
+//both kind of declaration mentioned above will give same return.[will create an empty object]
+
+tinderUser.id = "123abc"
+tinderUser.name = "denish"
+tinderUser.isLoggedin = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "damian@gmail.com",
+    fullName: {
+        userFullname: {
+            firstName: "Damian",
+            lastName: "Smith"
+        }
+    }
+}
+
+console.log(regularUser["fullName"]["userFullname"]["firstName"])
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+
+// const obj3 = {...obj1,...obj2}
+const obj3 = Object.assign({},obj1,obj2); 
+console.log(obj3)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
