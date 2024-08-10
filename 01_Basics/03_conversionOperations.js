72233f12b7e733704dc10465b693560440922401
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-10 23:54:51 +0530

    operations done

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -44,3 +44,24 @@ let str3 = str1 + str2;
 
 // console.log("1"+2+2)
 // console.log(1+2+"2")
+
+//****************not recommend to use in code.*****************
+
+// console.log(+true); //returns : 1
+// console.log(+""); //returns : 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+//prefix and postfix operator. [reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment]
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`); // Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`); // Expected output: "a:4, b:4"
+
+
+
+
