19ed608f967e1b11822bd2d48bff2a64446580bc
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-11 19:42:01 +0530

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -3,9 +3,18 @@ const repoCount = 17
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} nd my reo cunt is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //string interpolation OR template literal
 
-const gameName = new String('Binayak')
+const gameName = new String('Binayak')  // for better understanding of the available properties try to console the same in browser. 
+//Example
+console.log(gameName.length); //returns 7;
+console.log(gameName.toUpperCase()); //returns: BINAYAK
+console.log(gameName.charAt(3)); //returns: a
+console.log(gameName.indexOf('n')); //returns: 2
 
+const newString = gameName.substring(0,4);  
+console.log(newString); //returns: Bina
 
-// for better understanding of the available properties try to console the same in browser. 
+const anotherString = gameName.slice(-7,4);
+console.log(anotherString); //returns: Bina
+
