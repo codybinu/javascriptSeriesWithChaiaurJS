09e53d74168b7d3777bc85ff89ed917e2da41a85
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-11 13:30:49 +0530

    datatype summary

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -1,5 +1,12 @@
+//there is 2 types of datatypes, categorized as per the storing behaviour in memory and accessing them 
+// from memory
+
 // #primitive datatypes
     // 7 types: string, Number, Boolean, null, undefined, Symbol, BigInt
+
+/* 
+JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+*/
 
 const score = 100 //typeOf --> number
 const scoreValue = 100.3 //typeOf --> number
@@ -11,12 +18,14 @@ let userEmail;
 const id = Symbol('123') //typeOf --> symbol
 const anotherId = Symbol('123') //typeOf --> symbol
 
-// console.log(id)
-// console.log(anotherId)
+// console.log(id) //returns: Symbol(123)
+// console.log(anotherId) //returns: Symbol(123)
 
-// console.log(id === anotherId)
+// console.log(id === anotherId) //returns: false
 
 let bigNumber = 76234398478719n //typeOf --> bigint
+
+
 // #nonprimitives or reference types
     // Array,Objects, Functions
 
@@ -25,11 +34,21 @@ let myObj = {
     name: "Binayak",
     age: 29
 }                                       //typeOf --> object
+
 const myFunction = function(){          
     console.log("Hello World");
 }                                       //typeOf --> function
 
-// console.log(typeof myFunction);
+// console.log(typeof score);  // returns: number
+// console.log(typeof scoreValue); // returns: number
+// console.log(typeof isLoggedIn); // returns: boolean
+// console.log(typeof outSideTemp); // returns: object
+// console.log(typeof userEmail); // returns: undefined
+// console.log(typeof anotherId); // returns: symbol
+// console.log(typeof bigNumber); // returns: bigint
+// console.log(typeof heros); // returns: object
+// console.log(typeof myObj); // returns: object
+// console.log(typeof myFunction); // returns: function
 
 
 
