f10c89c84e692f5f5cf0cf83ded2029640ae5e3a
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-11 12:43:05 +0530

    comparisons

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -1,14 +1,24 @@
-// console.log("2" > 1)
-// console.log("02" > 1)
+// console.log("2" > 1)  //returns : true 
+// console.log("02" > 1) //returns : true
+//In the above comparison JS changing the "2" string datatype to number.
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+
+
+//***********Avoid using below type of comparisons************** 
+console.log(null > 0) //returns : false
+console.log(null == 0) //returns : false
+console.log(null >= 0) //returns : true
+
 // the reason is that an equality check  == and comparison > < >= <= works differently.
 // comparisons convert null to a number, treating it as 0.
 // that's why null>=0 is true and null>0 is false
 
+console.log(undefined == 0); //returns : false
+console.log(undefined > 0); //returns : false
+console.log(undefined < 0); //returns : false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+
+
+//*********How to use comparisons effectively *************
+// We use === to do strict check, which will check the values along with the datatype.
+console.log("2" === 2); //returns false
