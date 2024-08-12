698aecc58221bd46596326e49adf9d3895db0e76
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-12 09:57:39 +0530

    arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -1,3 +1,4 @@
+//learn more about arrays at : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 const myArr = [0,1,2,3,4]
 
 const myArr2 = new Array(1,2,3,4,5)
@@ -6,3 +7,9 @@ console.log(myArr2);
 // array methods
 myArr2.push(7)
 console.log(myArr2); // adds 7 to the end of the array
+
+/*
+Difference between splice and slice?
+1. Slice only includes starting index and exculdes ending index where splice includes both . 
+2. slice doesnot change the original array where as splice modifies the original array.
+*/
