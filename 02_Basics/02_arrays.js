7d77937e36e2a9da7e07f5bdfd1e6a14fb26e331
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-12 22:07:52 +0530

    array completed

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -3,28 +3,28 @@ const dc_heros = ["Superman","Flash","Batman"]
 
 // marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros)
+// console.log(marvel_heros) //output: [ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
 
-// let allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// let allHeros = marvel_heros.concat(dc_heros) 
+// console.log(allHeros); //output: [ 'Thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
 
 //spread operator
 let heroes = [...marvel_heros, ...dc_heros]
-// console.log(heroes)
+// console.log(heroes); //output: [ 'Thor', 'Ironman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
 
 const newArray = [1,2,3,[4,5],6,7,[8,9,[10,11,12]]]
 
 const usable_newArray = newArray.flat(Infinity);
 
-// console.log(usable_newArray);
+// console.log(usable_newArray); //output: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
 
 
-console.log(Array.isArray("Binayak"))
-console.log(Array.from("Binayak"))
-console.log(Array.from({name: "Binayak"})) //interesting
+console.log(Array.isArray("Binayak")); //output: false
+console.log(Array.from("Binayak")); //output: [ 'B', 'i', 'n', 'a', 'y', 'a', 'k' ]
+console.log(Array.from({name: "Binayak"})) //output: [] //interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+console.log(Array.of(score1,score2,score3)); //output: [ 100, 200, 300 ]
