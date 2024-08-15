c608b2694e5ae7121626b56385937a0cbc300e74
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-15 19:13:13 +0530

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -10,14 +10,27 @@ function addTwoNumbers(one,two){
 
 const result = addTwoNumbers(3,4)
 
-// console.log("Result: ",result);
+console.log("Result: ",result); //output: Result:  7
 
-function loginUserMessage(username = "sam") {
+ /*For the above example
+ addTwoNumbers --> function reference
+ addTwoNumbers() --> function execution
+ addTwoNumbers(){} --> function definition */
+
+ /*
+ -----Difference between parameters & arguments-----
+ --> the values passed inside paranthesis while defining the functions are called as parameters. EX: function example(a,b){};
+ --> the values passed inside paranthesis while calling back/ executing the functions are arguments. EX: example(a,b)
+ */ 
+
+
+function loginUserMessage(username = "sam") { //Here sam is the predefined/default username;
   if(!username){
     return "Please enter username.";
   }
-    return `${username} just loggedin`
+    return `${username} just loggedin`;
     
 }
 
-console.log(loginUserMessage("Binayak"))
+console.log(loginUserMessage()); //output: sam just loggedin
+console.log(loginUserMessage("Binayak")) //output: Binayak just loggedin
