1fe834a08fc989dc6ed62c7479154f76aebe0271
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-15 19:56:46 +0530

    scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,12 +1,23 @@
 
-////// var in scope concept starts from here
-if (true) {
+
+if (true) { //Whatever present in side curly braces are block scope. outside curly braces is Global scope
     let a = 10
     const b = 20
     var c = 30
 }
 
-// console.log(a)
-// console.log(b)
-console.log(c) // outside a block scope[globally] a variable from block scope should not be accessible.
-////// var in scope concept ends here
+// console.log(a) //output: Throws error with a message "a is not defined"
+// console.log(b) //output: Throws error with a message "b is not defined"
+console.log(c)//output: 30; // outside a block scope[globally] a variable from block scope should not be accessible.
+
+
+let a = 300;
+if(true){
+    let a = 10;
+    const b = 20;
+    console.log(`INNER: ${a}`) // output: INNER: 10
+}
+console.log(`OUTER: ${a}`) //output: OUTER: 300
+
+//IMPORTANT NOTE:
+//GLOBAL SCOPE in node[codes written here in code editor] is different that the code written in console window of browser.
