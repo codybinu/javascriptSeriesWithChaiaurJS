5da7c728fd84089a05e6fa89564d00736890a7ef
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-17 19:58:39 +0530

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+//Without "break" statements, all the below code of the matched case will get executed except default case.
