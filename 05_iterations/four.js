e2a6ba242a61135f6ad8674684087b9f7bd3519e
codybinu <binayak.bidyasagar99@gmail.com>
2024-08-18 22:21:35 +0530

    basics end

---
+++ 05_iterations/four.js
@@ -0,0 +1,40 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+//for in
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    /*output:
+        js shortcut is for javascript
+        cpp shortcut is for C++
+        rb shortcut is for ruby
+        swift shortcut is for swift by apple
+    */
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+    /*output:
+        js
+        rb
+        py
+        java
+        cpp 
+    */
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -6,7 +6,7 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
     /*output:
     1
     2
@@ -18,7 +18,21 @@ for (const num of arr) {
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
+    /*OUTPUT:
+    Each char is H
+    Each char is e
+    Each char is l
+    Each char is l
+    Each char is o
+    Each char is
+    Each char is w
+    Each char is o
+    Each char is r
+    Each char is l
+    Each char is d
+    Each char is !    
+    */
 }
 
 // Maps
@@ -30,10 +44,22 @@ map.set('Fr', "France")
 map.set('IN', "India")
 
 
-// console.log(map);
+// console.log(map); 
+/*output: 
+Map(3) {
+  'IN' => 'India',
+  'USA' => 'United States of America',
+  'Fr' => 'France'
+}
+*/
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
+    /*output:
+        IN :- India
+        USA :- United States of America
+        Fr :- France
+    */
 }
 
 const myObject = {
@@ -41,7 +67,7 @@ const myObject = {
     game2: 'Spiderman'
 }
 
-// for (const [key, value] of myObject) {
-//     console.log(key, ':-', value);
+for (const [key, value] of myObject) {
+    // console.log(key, ':-', value); // Throws an error with msg ---> "myObject is not iterable"
     
-// }
+}
